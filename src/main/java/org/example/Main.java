e734f6ad26df6f583a5cc5cc589aa17b913b9789
David-Navarro-Oliver <navarropalle@gmail.com>
2026-03-10 19:21:23 +0100

    feat: print calculator results in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Calculator TDD project");
+        Calculator calculator = new Calculator();
+
+        double firstNumber = 10;
+        double secondNumber = 5;
+
+        System.out.println("Addition: " + calculator.add(firstNumber, secondNumber));
+        System.out.println("Subtraction: " + calculator.subtract(firstNumber, secondNumber));
+        System.out.println("Multiplication: " + calculator.multiply(firstNumber, secondNumber));
+        System.out.println("Division: " + calculator.divide(firstNumber, secondNumber));
     }
 }
