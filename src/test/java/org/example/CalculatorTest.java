d150010cbd646f08826b5db33ceb05b20826667b
David-Navarro-Oliver <navarropalle@gmail.com>
2026-03-10 14:34:57 +0100

    chore: initialize java calculator project

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+    @Test
+    void shouldCreateCalculatorInstance() {
+        Calculator calculator = new Calculator();
+    }
+}
