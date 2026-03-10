b6645ff1c453367cdc1f58e044ff341cb66e433c
David-Navarro-Oliver <navarropalle@gmail.com>
2026-03-10 18:22:57 +0100

    feat: implement add method

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class Calculator {
+
+    public double add(double firstNumber, double secondNumber) {
+        return firstNumber + secondNumber;
+    }
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -2,10 +2,16 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CalculatorTest {
 
     @Test
-    void shouldCreateCalculatorInstance() {
+    void shouldAddTwoPositiveNumbers() {
         Calculator calculator = new Calculator();
+
+        double result = calculator.add(10, 5);
+
+        assertEquals(15, result);
     }
 }
